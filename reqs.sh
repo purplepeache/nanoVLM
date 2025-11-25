6a3ba3dc7be2193a70b5ee930cda07758f4fff12
root <ren>
2025-11-25 06:36:11 +0000

    .

---
+++ reqs.sh
@@ -0,0 +1,4 @@
+pip install pandas matplotlib seaborn scikit-learn
+
+pip install transformers datasets scikit-learn torch accelerate huggingface_hub hf_transfer bitsandbytes
+pip install flash-attn --no-build-isolation
